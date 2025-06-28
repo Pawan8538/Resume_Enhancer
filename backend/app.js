acde63f5fa379b69916c15e327edcd8b30c60b94
Pawan Patidar <pawanpatidar8538@gmail.com>
2025-06-28 14:31:52 +0530

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,11 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://resume-enhancer-frontend.onrender.com",
+  methods: ["GET", "POST"],
+}));
+
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
